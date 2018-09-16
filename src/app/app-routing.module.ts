0b40d3a7bac7e6f24ff64bca94c5684e22b41954
Mayra <mayrarodriguez@rovistack.com>
2018-09-16 10:18:16 -0500

    dashboard link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-import { LandingComponent } from './landing/landing.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
   {
     path: 'landing',
     loadChildren: 'src/app/landing/landing.module#LandingModule'
+  },
+  {
+    path: 'dashboard',
+    loadChildren: 'src/app/dashboard/dashboard.module#DashboardModule'
   },
   {
     path: '',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { LandingComponent } from './landing/landing.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    // LandingComponent
-    PageNotFoundComponent
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
