4b61fb32294a79d2b29304ce0fc8ed07baa0409d
Mayra <mayrarodriguez@rovistack.com>
2018-09-16 10:24:04 -0500

    auth components

--- src/app/landing/landing.module.ts
+++ src/app/landing/landing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingComponent } from './landing.component';
 import { LandingRoutingModule } from './landing-routing.module';
+import { AuthModule } from '../auth/auth.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    LandingRoutingModule
+    LandingRoutingModule,
+    AuthModule
   ],
   declarations: [LandingComponent]
 })
